0fa46bfe4d38a8853ddfa6c8e4171dcb8594dc66
KangDongHyuk <kkddeerr@naver.com>
2020-03-20 10:26:33 +0900

    init

---
+++ src/main/java/com/kdh/spring/securitysessionwebsocket/SecuritySessionWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.kdh.spring.securitysessionwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySessionWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritySessionWebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kdh/spring/securitysessionwebsocket/SecuritySessionWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdh.spring.securitysessionwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySessionWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
